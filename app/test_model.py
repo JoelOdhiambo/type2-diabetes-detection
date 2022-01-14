08f8d80067b2cde55d3d949a5a8df319a6fa3b4b
JoelOdhiambo <wesleyjoel4@gmail.com>
2022-01-14 13:19:26 +0300

    Docker deployment

---
+++ app/test_model.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import numpy as np
+
+if __name__ == "__main__":
+    X = np.random.uniform(0, 1, (1, 8 ))
+
+    input_data = json.dumps({
+        "signature_name": "serving_default",
+        "instances": X.tolist(),
+    })
+
+    SERVER_URL = 'http://localhost:8501/v1/models/best_nn_model:predict'
+
+    response = requests.post(SERVER_URL, data=input_data)
+    response.raise_for_status() # raise an exception in case of error
+    response = response.json()
+
+    print(response)
